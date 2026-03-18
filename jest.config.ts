8c2da88fb15d3dc230c251732fe1afab7c55cd8a
Yusif Aliyev <114253515+YusifAliyevPro@users.noreply.github.com>
2026-03-18 12:21:41 +0400

    chore: remove moduleNameMapper from jest config and update dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -3,9 +3,6 @@ import type { Config } from "jest";
 const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
-  moduleNameMapper: {
-    "^@yusifaliyevpro/countries$": "<rootDir>/dist/index.cjs",
-  },
 };
 
 export default config;
